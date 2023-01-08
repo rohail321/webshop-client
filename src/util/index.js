f124615a0ba7be24446b457e52e208a5f1657179
rohail321 <qrohail32@gmail.com>
2023-01-08 11:01:17 +0000

    reactfinal1

--- src/util/index.js
+++ src/util/index.js
@@ -4,7 +4,7 @@ const isDevelopment=window.location.hostname.includes("localhost");
 
 
 const getServer=()=>{
-    return  isDevelopment?"http://localhost:5000":""
+    return  isDevelopment?"http://localhost:5000":"https://steel-math-374002.uc.r.appspot.com/"
 }
 const decodeUser=()=>{
     const token=localStorage.getItem("token")
